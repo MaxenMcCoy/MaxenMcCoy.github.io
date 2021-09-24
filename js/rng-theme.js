a6d994c311e18680839739d07a6ccd8a5857c270
Maxen McCoy <maxen.mccoy@skymap.com>
2021-09-24 19:04:10 -0400

    New background themes + updated resume

--- js/rng-theme.js
+++ js/rng-theme.js
@@ -2,7 +2,13 @@ var themes = [
     {image: "images/PEAK/peakBKG.jpg", color: "#ff0094"},
     {image: "images/spacePizza/space-pizza-bkg.jpg", color: "#bd3536"},
 	{image: "images/emerald/background.jpg", color: "#43aa55"},
-	{image: "images/zombies/multiplayer-zombies.jpg", color: "#f3e0a3"}
+	{image: "images/zombies/multiplayer-zombies.jpg", color: "#f3e0a3"},
+	{image: "images/miscBackgrounds/delugeional.jpg", color: "#c24651"},
+	{image: "images/miscBackgrounds/signalsInDust.jpg", color: "#f7c83c"},
+	{image: "images/miscBackgrounds/gunball.jpg", color: "#FF6625"},
+	{image: "images/miscBackgrounds/goap.jpg", color: "#d4bf83"},
+	{image: "images/miscBackgrounds/floorsOfFright.jpg", color: "#bed600"},
+	{image: "images/miscBackgrounds/CityByTheSea.jpg", color: "#c2adff"}
 ]
 
 $(function() {
